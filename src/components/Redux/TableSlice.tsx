e6c6be9c02950f04486720a1d98ede72b551fe9e
DevEmKen <emken1997@gmail.com>
2024-02-02 17:11:09 -0800

    Finished table. Now to implement google login and database capabilities, to save files and table contents.

--- src/components/Redux/TableSlice.tsx
+++ src/components/Redux/TableSlice.tsx
@@ -7,12 +7,32 @@ const firstState: TableStateType = {
   tableArr: [],
 };
 
+const customSort = (property: string) => {
+  var ascDesc = 1;
+  if (property[0] === "-") {
+    ascDesc = -1;
+    property = property.substring(1);
+  }
+  return function (a: any, b: any) {
+    var result =
+      a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+    return result * ascDesc;
+  };
+};
+
 export const TableSlice = createSlice({
   name: "TableSlice",
   initialState: firstState,
   reducers: {
     sortByProperty(state, action) {
       const property = action.payload;
+      if (state.sortedProperty === property) {
+        state.tableArr?.sort(customSort("-" + property));
+        state.sortedProperty = "-" + property;
+      } else {
+        state.tableArr?.sort(customSort(property));
+        state.sortedProperty = property;
+      }
     },
     setTableData(state, action) {
       state.tableArr = action.payload;
--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -5,6 +5,15 @@ import { UserType, FullUserType, TableStateType } from "./Types/TableTypes";
 import { useSelector, useDispatch } from "react-redux";
 import { sortByProperty, setTableData } from "./Redux/TableSlice";
 
+const tableTitles = [
+  { name: "First Name", sortId: "firstName", styling: { width: "150px" } },
+  { name: "Last Name", sortId: "lastName", styling: { width: "150px" } },
+  { name: "City", sortId: "city", styling: { width: "150px" } },
+  { name: "State", sortId: "state", styling: { width: "150px" } },
+  { name: "Country", sortId: "country", styling: { width: "150px" } },
+  { name: "Age", sortId: "age", styling: { width: "150px" } },
+];
+
 const UserTable = () => {
   const fetchUsers = async (fetchAmount: number): Promise<FullUserType> => {
     const numberedURL = "https://randomuser.me/api/?results=" + fetchAmount;
@@ -39,14 +48,48 @@ const UserTable = () => {
     initializeTable();
   }, []);
 
+  const handleSort = (sortBy: string) => {
+    dispatch(sortByProperty(sortBy));
+  };
+
   const users = useSelector((state: any) => state.TableSlice);
   const dispatch = useDispatch();
 
   return (
     <>
-      {users.tableArr.map((user: UserType) => {
-        return <div>{user.firstName}</div>;
-      })}
+      <div className="table-properties">
+        {tableTitles.map((title) => (
+          <div
+            className="table-title"
+            style={title.styling}
+            onClick={() => handleSort(title.sortId)}
+          >
+            {title.name}
+          </div>
+        ))}
+      </div>
+      {users.tableArr.map((user: UserType) => (
+        <div className="table-row">
+          <div className="table-entry" style={{ width: "150px" }}>
+            {user.firstName}
+          </div>
+          <div className="table-entry" style={{ width: "150px" }}>
+            {user.lastName}
+          </div>
+          <div className="table-entry" style={{ width: "150px" }}>
+            {user.city}
+          </div>
+          <div className="table-entry" style={{ width: "150px" }}>
+            {user.state}
+          </div>
+          <div className="table-entry" style={{ width: "150px" }}>
+            {user.country}
+          </div>
+          <div className="table-entry" style={{ width: "150px" }}>
+            {user.age}
+          </div>
+        </div>
+      ))}
     </>
   );
 };
